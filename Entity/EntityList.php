38cf0d9824f37b113c0553aa8890affe1e66b178
Nath_ <firminchanteux@gmail.com>
2020-12-09 14:32:12 +0100

    update

---
+++ Entity/EntityList.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace nlib\Orm\Entity;
+
+use Nlib\ObjectList\Classes\ObjectList;
+
+class EntityList extends ObjectList { }
---
+++ Entity/ResultList.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace nlib\Orm\Entity;
+
+use Nlib\ObjectList\Classes\ObjectList;
+
+class ResultList extends ObjectList { }
